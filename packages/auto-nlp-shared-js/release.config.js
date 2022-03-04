225805a31c306e82d3a56dac3874fbfd8a8088b3
Christian Gawron <gawron.christian@fh-swf.de>
2022-03-04 22:21:56 +0100

    chore: add release.config.js

---
+++ packages/auto-nlp-shared-js/release.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    // "@semantic-release/npm",
+    '@semantic-release/git',
+  ],
+  extends: 'semantic-release-monorepo',
+  preset: 'angular',
+  branches: ['main'],
+};
