a5f878c4b8dc914035e18aafedc1d03550252778
Christian Gawron <gawron.christian@fh-swf.de>
2022-03-23 10:28:00 +0100

    chore(ci): fix ci test

--- packages/auto-nlp-shared-js/jest.config.js
+++ packages/auto-nlp-shared-js/jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  resolver: require.resolve(`jest-pnp-resolver`)
+  resolver: require.resolve(`jest-pnp-resolver`),
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  reporters: ['default', 'jest-junit'],
+  testRegex: '.*\\.spec\\.ts$',
+
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
 };
--- packages/auto-nlp-workflow-lib/release.config.js
+++ packages/auto-nlp-workflow-lib/release.config.js
@@ -4,6 +4,13 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/git',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/github',
+      {
+        assets: ['CHANGELOG.md'],
+      },
+    ],
   ],
   extends: 'semantic-release-monorepo',
   preset: 'angular',
