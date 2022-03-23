d336a3d543f8101e7650027f695d75d3078a8e61
Christian Gawron <gawron.christian@fh-swf.de>
2022-03-23 22:14:34 +0100

    chore(deps): fix dependencies chore(ci): fix release config

--- packages/auto-nlp-huggingface-search/release.config.js
+++ packages/auto-nlp-huggingface-search/release.config.js
@@ -1,9 +1,18 @@
 module.exports = {
+  //useFactory: databaseConfigFactory,
+
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
+    '@semantic-release/npm',
     '@semantic-release/git',
+    [
+      '@semantic-release/github',
+      {
+        assets: ['CHANGELOG.md'],
+      },
+    ],
   ],
   extends: 'semantic-release-monorepo',
   preset: 'angular',
