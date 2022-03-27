1bb3e7452747e9ab5ca95408004fe121ad68d1d0
Christian Gawron <gawron.christian@fh-swf.de>
2022-03-27 22:54:26 +0200

    chore(CI/CD): make docker deployment configurable - Goal: Build stack via GitHub - Move all secrets to environment so that config can be   published on GitHub - Make all port numbers configurable via environment to   allow multiple deployments using one   `docker-compose.yml`

--- packages/auto-nlp-deployment/src/docker_repositories/docker_repository_config.py
+++ packages/auto-nlp-deployment/src/docker_repositories/docker_repository_config.py
@@ -1,8 +1,11 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, BaseSettings, Field
 
 
-class DockerRepositoryConfig(BaseModel):
-    name: str = Field(None, description="Name of the docker repository config.")
+class DockerRepositoryConfig(BaseSettings):
+    name: str = Field(
+        None, description="Name of the docker repository config.")
     url: str = Field(None, description="Url to the docker repository.")
-    username: str = Field(None, description="Username for login to docker repository.")
-    password: str = Field(None, description="Password for login to docker repository.")
+    username: str = Field(
+        None, description="Username for login to docker repository.")
+    password: str = Field(
+        None, description="Password for login to docker repository.", env="DOCKER_PASSWORD")
