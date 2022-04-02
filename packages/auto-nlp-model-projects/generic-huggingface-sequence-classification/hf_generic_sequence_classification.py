764dc8f8029bb16251d3859daf4631a70d2a4b81
Christian Gawron <gawron.christian@fh-swf.de>
2022-04-02 14:51:34 +0200

    fix(training): use GPU for training (#29)
    - Remove explicit setting of CUDA_AVAILABLE_DEVICES in training script.
      In trainings started via mlflow in docker, this conflicts with the external
      settings. This results in training running on CPU only.

--- packages/auto-nlp-model-projects/generic-huggingface-sequence-classification/hf_generic_sequence_classification.py
+++ packages/auto-nlp-model-projects/generic-huggingface-sequence-classification/hf_generic_sequence_classification.py
@@ -28,7 +28,6 @@ from tagflip.model.mlflow.huggingface import HuggingFaceSequenceClassificationSa
 
 logger = logging.getLogger(__name__)
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 @dataclass
 class ModelArguments:
@@ -36,76 +35,84 @@ class ModelArguments:
     Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
     """
     model_name: str = field(
-            metadata={"help": "Name of pretrained model or model identifier from huggingface.co/models"},
+        metadata={
+            "help": "Name of pretrained model or model identifier from huggingface.co/models"},
     )
     config_name: Optional[str] = field(
-            default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
+        default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
     )
     tokenizer_name: Optional[str] = field(
-            default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
     )
     model_revision: str = field(
-            default="main",
-            metadata={"help": "The specific model version to use (can be a branch name, tag name or commit id)."},
+        default="main",
+        metadata={
+            "help": "The specific model version to use (can be a branch name, tag name or commit id)."},
     )
     use_auth_token: bool = field(
-            default=False,
-            metadata={
-                "help": "Will use the token generated when running `transformers-cli login` (necessary to use this script "
-                        "with private models)."
-            },
+        default=False,
+        metadata={
+            "help": "Will use the token generated when running `transformers-cli login` (necessary to use this script "
+            "with private models)."
+        },
     )
+
 
 @dataclass
 class DataTrainingArguments:
     """
     Arguments pertaining to what data we are going to input our model for training and eval.
     """
-    task_name: Optional[str] = field(default="sc", metadata={"help": "The name of the task (ner, pos...)."})
+    task_name: Optional[str] = field(
+        default="sc", metadata={"help": "The name of the task (ner, pos...)."})
     pad_to_max_length: bool = field(
-            default=False,
-            metadata={
-                "help": "Whether to pad all samples to model maximum sentence length. "
-                        "If False, will pad the samples dynamically when batching to the maximum length in the batch. More "
-                        "efficient on GPU but very bad for TPU."
-            },
+        default=False,
+        metadata={
+            "help": "Whether to pad all samples to model maximum sentence length. "
+            "If False, will pad the samples dynamically when batching to the maximum length in the batch. More "
+            "efficient on GPU but very bad for TPU."
+        },
     )
     max_train_samples: Optional[int] = field(
-            default=None,
-            metadata={
-                "help": "For debugging purposes or quicker training, truncate the number of training examples to this "
-                        "value if set."
-            },
+        default=None,
+        metadata={
+            "help": "For debugging purposes or quicker training, truncate the number of training examples to this "
+            "value if set."
+        },
     )
     max_val_samples: Optional[int] = field(
-            default=None,
-            metadata={
-                "help": "For debugging purposes or quicker training, truncate the number of validation examples to this "
-                        "value if set."
-            },
+        default=None,
+        metadata={
+            "help": "For debugging purposes or quicker training, truncate the number of validation examples to this "
+            "value if set."
+        },
     )
     max_test_samples: Optional[int] = field(
-            default=None,
-            metadata={
-                "help": "For debugging purposes or quicker training, truncate the number of test examples to this "
-                        "value if set."
-            },
+        default=None,
+        metadata={
+            "help": "For debugging purposes or quicker training, truncate the number of test examples to this "
+            "value if set."
+        },
     )
     return_entity_level_metrics: bool = field(
-            default=False,
-            metadata={"help": "Whether to return all the entity levels during evaluation or just the overall ones."},
+        default=False,
+        metadata={
+            "help": "Whether to return all the entity levels during evaluation or just the overall ones."},
     )
 
     def __post_init__(self):
         self.task_name = self.task_name.lower()
 
+
 @dataclass
 class HyperSpaceTuneArgs:
-    trials: int = field(default=1, metadata={"help": "The number of trials that should be performed."})
+    trials: int = field(default=1, metadata={
+                        "help": "The number of trials that should be performed."})
     learning_rate_min_max: Tuple[float, float] = field(default=(1e-6, 1e-4), metadata={
         "help": "The minimum and maximum value to sample from uniform distribution of given ranges"})
 
-    max_num_train_epochs: int = field(default=1, metadata={"help": "The maximum number of train epochs"})
+    max_num_train_epochs: int = field(
+        default=1, metadata={"help": "The maximum number of train epochs"})
 
     seed_max: int = field(default=40, metadata={
         "help": "The maximum value to sample from uniform distribution of range [1,seed_max]"})
@@ -117,7 +124,8 @@ class HyperSpaceTuneArgs:
 class HFGenericSequenceClassificationWorkflow(HuggingFaceWorkflow):
 
     def __init__(self):
-        super().__init__((TrainingArguments, ModelArguments, DataTrainingArguments), HyperSpaceTuneArgs)
+        super().__init__((TrainingArguments, ModelArguments,
+                          DataTrainingArguments), HyperSpaceTuneArgs)
 
     def train(self, datasets: DatasetDict, autonlp_args: AutoNLPArguments, args: Dict[ArgsType, Args],
               hyperspace_tune_args: HyperSpaceTuneArgs = None):
@@ -132,12 +140,13 @@ class HFGenericSequenceClassificationWorkflow(HuggingFaceWorkflow):
         logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
                             datefmt="%m/%d/%Y %H:%M:%S",
                             handlers=[logging.StreamHandler(sys.stdout)])
-        logger.setLevel(logging.INFO if is_main_process(training_args.local_rank) else logging.WARN)
+        logger.setLevel(logging.INFO if is_main_process(
+            training_args.local_rank) else logging.WARN)
 
         # Log on each process the small summary:
         logger.warning(
-                f"Process rank: {training_args.local_rank}, device: {training_args.device}, n_gpu: {training_args.n_gpu}"
-                + f"distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}"
+            f"Process rank: {training_args.local_rank}, device: {training_args.device}, n_gpu: {training_args.n_gpu}"
+            + f"distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}"
         )
         # Set the verbosity to info of the Transformers logger (on main process only):
         if is_main_process(training_args.local_rank):
@@ -149,7 +158,7 @@ class HFGenericSequenceClassificationWorkflow(HuggingFaceWorkflow):
         # Set seed before initializing model.
         set_seed(training_args.seed)
 
-        #get column names
+        # get column names
         column_names = train_dataset.column_names
         features = train_dataset.features
 
@@ -170,7 +179,7 @@ class HFGenericSequenceClassificationWorkflow(HuggingFaceWorkflow):
             label_list = features[label_column_name].names
         else:
             label_list = get_label_list(features[label_column_name])
-            
+
         num_labels = len(label_list)
 
         # Load pretrained model and tokenizer
@@ -179,32 +188,33 @@ class HFGenericSequenceClassificationWorkflow(HuggingFaceWorkflow):
         # The .from_pretrained methods guarantee that only one local process can concurrently
         # download model & vocab.
         config = AutoConfig.from_pretrained(
-                model_args.config_name if model_args.config_name else model_args.model_name,
-                num_labels=num_labels,
-                finetuning_task=data_args.task_name,
-                revision=model_args.model_revision,
-                use_auth_token=True if model_args.use_auth_token else None,
+            model_args.config_name if model_args.config_name else model_args.model_name,
+            num_labels=num_labels,
+            finetuning_task=data_args.task_name,
+            revision=model_args.model_revision,
+            use_auth_token=True if model_args.use_auth_token else None,
         )
         tokenizer = AutoTokenizer.from_pretrained(
-                model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name,
-                use_fast=True,
-                revision=model_args.model_revision,
-                use_auth_token=True if model_args.use_auth_token else None
+            model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name,
+            use_fast=True,
+            revision=model_args.model_revision,
+            use_auth_token=True if model_args.use_auth_token else None
         )
+
         def model_init():
             return AutoModelForSequenceClassification.from_pretrained(
-                    model_args.model_name,
-                    config=config,
-                    revision=model_args.model_revision,
-                    use_auth_token=True if model_args.use_auth_token else None
+                model_args.model_name,
+                config=config,
+                revision=model_args.model_revision,
+                use_auth_token=True if model_args.use_auth_token else None
             )
 
         # Tokenizer check: this script requires a fast tokenizer.
         if not isinstance(tokenizer, PreTrainedTokenizerFast):
             raise ValueError(
-                    "This script only works for models that have a fast tokenizer. Checkout the big table of models "
-                    "at https://huggingface.co/transformers/index.html#bigtable to find the model types that meet this "
-                    "requirement"
+                "This script only works for models that have a fast tokenizer. Checkout the big table of models "
+                "at https://huggingface.co/transformers/index.html#bigtable to find the model types that meet this "
+                "requirement"
             )
 
         # Preprocessing the dataset
@@ -214,24 +224,27 @@ class HFGenericSequenceClassificationWorkflow(HuggingFaceWorkflow):
         # Tokenize text
         def tokenized_data(examples):
             tokenized_datasets = tokenizer(
-                    examples[text_column_name],
-                    padding=padding,  # whether to pad to max seq length or not
-                    truncation=True
+                examples[text_column_name],
+                padding=padding,  # whether to pad to max seq length or not
+                truncation=True
             )
             return tokenized_datasets
 
         if data_args.max_train_samples is not None:
-            train_dataset = train_dataset.select(range(data_args.max_train_samples))
+            train_dataset = train_dataset.select(
+                range(data_args.max_train_samples))
         train_dataset = train_dataset.map(tokenized_data, batched=True)
 
         if data_args.max_val_samples is not None:
-            valid_dataset = valid_dataset.select(range(data_args.max_val_samples))
+            valid_dataset = valid_dataset.select(
+                range(data_args.max_val_samples))
         valid_dataset = valid_dataset.map(tokenized_data, batched=True)
 
         test_dataset = test_dataset.map(tokenized_data, batched=True)
 
         # Data collator
-        data_collator = DataCollatorWithPadding(tokenizer, pad_to_multiple_of=8 if training_args.fp16 else None)
+        data_collator = DataCollatorWithPadding(
+            tokenizer, pad_to_multiple_of=8 if training_args.fp16 else None)
 
         # Metrics
         metrics_list = ['accuracy', 'f1', 'precision', 'recall']
@@ -244,7 +257,8 @@ class HFGenericSequenceClassificationWorkflow(HuggingFaceWorkflow):
             predictions = np.argmax(predictions, axis=-1)
             final_results = {}
             for i in range(len(metrics_list)):
-                results = metric[i].compute(predictions=predictions, references=labels)
+                results = metric[i].compute(
+                    predictions=predictions, references=labels)
                 for key, value in results.items():
                     if isinstance(value, dict):
                         for n, v in value.items():
@@ -254,19 +268,21 @@ class HFGenericSequenceClassificationWorkflow(HuggingFaceWorkflow):
             return final_results
 
         trainer = Trainer(
-                model_init=model_init,
-                args=training_args,
-                train_dataset=train_dataset,
-                eval_dataset=valid_dataset,
-                tokenizer=tokenizer,
-                data_collator=data_collator,
-                compute_metrics=compute_metrics,
+            model_init=model_init,
+            args=training_args,
+            train_dataset=train_dataset,
+            eval_dataset=valid_dataset,
+            tokenizer=tokenizer,
+            data_collator=data_collator,
+            compute_metrics=compute_metrics,
         )
-        trainer.add_callback(self.get_hf_tagflip_trainer_callback())  # this is important to attach tagflip capabilities
+        # this is important to attach tagflip capabilities
+        trainer.add_callback(self.get_hf_tagflip_trainer_callback())
 
         logger.info("*** Evaluate untrained ***")
         metrics = trainer.evaluate()
-        max_test_samples = data_args.max_test_samples if data_args.max_test_samples is not None else len(valid_dataset)
+        max_test_samples = data_args.max_test_samples if data_args.max_test_samples is not None else len(
+            valid_dataset)
         metrics["eval_samples"] = min(max_test_samples, len(valid_dataset))
         trainer.log_metrics("eval", metrics)
         self.log_metrics(metrics)
@@ -280,31 +296,37 @@ class HFGenericSequenceClassificationWorkflow(HuggingFaceWorkflow):
                                                           hyperspace_tune_args.max_num_train_epochs),
                     "seed": trial.suggest_int("seed", 1, hyperspace_tune_args.seed_max),
                     "per_device_train_batch_size": trial.suggest_categorical("per_device_train_batch_size", list(
-                            hyperspace_tune_args.possible_train_batch_sizes)),
+                        hyperspace_tune_args.possible_train_batch_sizes)),
                 }
 
-            logger.info(f"Starting training with automatic hyperparameter search. There will be {hyperspace_tune_args.trials} trials.")
+            logger.info(
+                f"Starting training with automatic hyperparameter search. There will be {hyperspace_tune_args.trials} trials.")
             best_run = trainer.hyperparameter_search(hp_space_fn, backend="optuna",
                                                      compute_objective=lambda x: x['eval_f1'],
                                                      direction="maximize",
                                                      n_trials=hyperspace_tune_args.trials)
-            model_dir = os.path.join(training_args.output_dir, f"run-{best_run.run_id}")
+            model_dir = os.path.join(
+                training_args.output_dir, f"run-{best_run.run_id}")
             logger.info(f"Loading best model from {model_dir}.")
-            last_checkpoint_dir = sorted(glob(os.path.join(model_dir, "*", "")))[-1]
-            model = AutoModelForSequenceClassification.from_pretrained(last_checkpoint_dir)
+            last_checkpoint_dir = sorted(
+                glob(os.path.join(model_dir, "*", "")))[-1]
+            model = AutoModelForSequenceClassification.from_pretrained(
+                last_checkpoint_dir)
             tmp_trainer = Trainer(
-                    model=model,
-                    args=training_args,
-                    train_dataset=train_dataset,
-                    eval_dataset=valid_dataset,
-                    tokenizer=tokenizer,
-                    data_collator=data_collator,
-                    compute_metrics=compute_metrics,
+                model=model,
+                args=training_args,
+                train_dataset=train_dataset,
+                eval_dataset=valid_dataset,
+                tokenizer=tokenizer,
+                data_collator=data_collator,
+                compute_metrics=compute_metrics,
             )
-            tmp_trainer.add_callback(self.get_hf_tagflip_trainer_callback())  # this is important to attach tagflip capabilities
+            # this is important to attach tagflip capabilities
+            tmp_trainer.add_callback(self.get_hf_tagflip_trainer_callback())
             logger.info("*** Evaluate best found model ***")
             metrics = tmp_trainer.evaluate()
-            max_test_samples = data_args.max_test_samples if data_args.max_test_samples is not None else len(valid_dataset)
+            max_test_samples = data_args.max_test_samples if data_args.max_test_samples is not None else len(
+                valid_dataset)
             metrics["eval_samples"] = min(max_test_samples, len(valid_dataset))
             tmp_trainer.log_metrics("eval", metrics)
             self.log_metrics(metrics)
@@ -322,9 +344,11 @@ class HFGenericSequenceClassificationWorkflow(HuggingFaceWorkflow):
             train_result = trainer.train()
             metrics = train_result.metrics
             max_train_samples = (
-                data_args.max_train_samples if data_args.max_train_samples is not None else len(train_dataset)
+                data_args.max_train_samples if data_args.max_train_samples is not None else len(
+                    train_dataset)
             )
-            metrics["train_samples"] = min(max_train_samples, len(train_dataset))
+            metrics["train_samples"] = min(
+                max_train_samples, len(train_dataset))
             trainer.log_metrics("train", metrics)
             self.log_metrics(metrics)
             trainer.save_state()
